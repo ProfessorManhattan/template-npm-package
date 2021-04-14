6fb12f2b86c446cee262f1c38a5205ba0e3cc513
Brian Zalewski <brian@megabyte.space>
2021-04-13 21:04:05 -0400

    [skip ci] Added base files.

---
+++ src/cli.ts
@@ -0,0 +1,7 @@
+/**
+ * This is where the logic that handles the CLI should reside
+ * 
+ * @param argz The command line arguments passed to the program
+ */
+export async function cli(argz: any): Promise<void> {
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/**
+ * This is where we export public methods that other
+ * programmers can import into their project if this
+ * project is intended to have a public API
+ */
