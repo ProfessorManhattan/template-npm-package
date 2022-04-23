a28e1c0a8520595d83cb17e569dcd416d023b0d8
Brian Zalewski <brian@megabyte.space>
2022-04-23 02:27:29 -0400

    🔧 chore(latest): latest

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
-
 /**
  * This is where the app's logic should reside.
  *
  * See https://gitlab.com/megabyte-space/npm/buildr for examples
  */
-export class AppName {
-
-}
+export class AppName {}
